fb77434ebf178687688cefa96f1353cfc453d3cf
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-09 13:57:02 +0600

    total Sales

---
+++ Problem-solving/findtotalcost.js
@@ -0,0 +1,28 @@
+// Calculate the Total sale of the three item with given price, quantity and conditions
+
+function totalSales(shirtQuantity, pantQuantity, shoeQuantity) {
+    if (perShirtQuantity < 1 && perPantQuantity < 1 && perShoeQuantity < 1) {
+        output = "Invalid input. Please state your quntity.";
+        return output;
+    }
+    else {
+        let shirtPrice = 100;
+        let pantPrice = 200;
+        let shoePrice = 500;
+
+        let shirtItemPrice = shirtQuantity * shirtPrice;
+        let pantItemPrice = pantQuantity * pantPrice;
+        let shoeItemPrice = shoeQuantity * shoePrice;
+
+        let totalItemSale = shirtItemPrice + pantItemPrice + shoeItemPrice;
+
+        return totalItemSale;
+    }
+}
+
+let perShirtQuantity = 1;
+let perPantQuantity = 1;
+let perShoeQuantity = 1;
+
+let myTotalSales = totalSales(perShirtQuantity, perPantQuantity, perShoeQuantity);
+console.log(myTotalSales);
