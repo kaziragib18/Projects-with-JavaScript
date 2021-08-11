64909021df446e3a4e77330392851ae666a9cf63
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-11 20:20:36 +0600

    function added

--- Problem-solving/Search-Product.js
+++ Problem-solving/Search-Product.js
@@ -1,11 +1,19 @@
 const product = [
-    { name: "Poco f1", price: 33000 },
+    { name: "Poco phone f1", price: 33000 },
+    { name: "I phone 11", price: 75000 },
     { name: "HP Laptop", price: 53000 },
+    { name: "Asus Laptop", price: 43000 },
     { name: "Bose Headphone", price: 21000 },
     { name: "Samsung TV", price: 44000 },
     { name: "Xiaomi Smart Watch", price: 13000 },
     { name: "Go Pro Camera", price: 25000 },
 ]
-function searchProducts(products, searchString){
-    
+function searchProducts(products, searchString) {
+    for (const product of products) {
+        if (product.name.indexOf(searchString) != -1) {
+ 
+        }
+    }
 }
+const matched = searchProducts(products, "Laptop");
+console.log(matched);
