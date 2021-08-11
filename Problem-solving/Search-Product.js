6ad883e888af39c8a2ffb65d5bc71e8d8f6cecd2
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-11 20:24:33 +0600

    function fixed

--- Problem-solving/Search-Product.js
+++ Problem-solving/Search-Product.js
@@ -4,16 +4,20 @@ const product = [
     { name: "HP Laptop", price: 53000 },
     { name: "Asus Laptop", price: 43000 },
     { name: "Bose Headphone", price: 21000 },
-    { name: "Samsung TV", price: 44000 },
+    { name: "Samsung Tv", price: 46000 },
+    { name: "Sony Tv", price: 45000 },
     { name: "Xiaomi Smart Watch", price: 13000 },
     { name: "Go Pro Camera", price: 25000 },
 ]
 function searchProducts(products, searchString) {
+    const matched = [];
     for (const product of products) {
-        if (product.name.indexOf(searchString) != -1) {
- 
+        const name = product.name;
+        if (name.indexOf(searchString) != -1) {
+            matched.push(product);
         }
     }
+    return matched;
 }
 const matched = searchProducts(products, "Laptop");
 console.log(matched);
