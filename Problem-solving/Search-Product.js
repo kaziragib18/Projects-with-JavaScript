2251e84d3739b14e9fd7500af41a131de199b2f9
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-11 20:07:11 +0600

    search product

---
+++ Problem-solving/Search-Product.js
@@ -0,0 +1,11 @@
+const product = [
+    { name: "Poco f1", price: 33000 },
+    { name: "HP Laptop", price: 53000 },
+    { name: "Bose Headphone", price: 21000 },
+    { name: "Samsung TV", price: 44000 },
+    { name: "Xiaomi Smart Watch", price: 13000 },
+    { name: "Go Pro Camera", price: 25000 },
+]
+function searchProducts(products, searchString){
+    
+}
