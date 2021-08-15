7f10235f7ab6a16fc4e28d60e9f0466b8d698239
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-15 14:37:36 +0600

    Balance update

--- Banking System/js/banking.js
+++ Banking System/js/banking.js
@@ -1,12 +1,31 @@
 // handle deposit button event
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    //getv the amount deposited
+    //get the amount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
-    
+
+    //converting input string to number
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+  
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+
+    //converting input string to number
+     const previousDepositText = depositTotal.innerText;
+     const previousDepositAmount = parseFloat(previousDepositText);
+    //Update Total deposit
+     const newDepositeTotal = previousDepositAmount + newDepositAmount;
+ 
+    depositTotal.innerText = newDepositeTotal;
+
+    //Update Balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
     //clear deposit input field
     depositInput.value = '';
 
