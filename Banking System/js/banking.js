31bd279d5077024481a1fdf9716e7bcc6bba170f
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-15 15:09:52 +0600

    withdaw ammout total  & upadate balance after withdraw

--- Banking System/js/banking.js
+++ Banking System/js/banking.js
@@ -6,27 +6,62 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     //converting input string to number
     const newDepositText = depositInput.value;
     const newDepositAmount = parseFloat(newDepositText);
-  
+
     const depositTotal = document.getElementById('deposit-total');
 
-    //converting input string to number
-     const previousDepositText = depositTotal.innerText;
-     const previousDepositAmount = parseFloat(previousDepositText);
+    //converting deposit input string to number
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
     //Update Total deposit
-     const newDepositeTotal = previousDepositAmount + newDepositAmount;
- 
+    const newDepositeTotal = previousDepositAmount + newDepositAmount;
     depositTotal.innerText = newDepositeTotal;
 
-    //Update Balance
+    //Update Balance after deposit
     const balanceTotal = document.getElementById('balance-total');
+
+    //converting balance input string to number
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    //Update Total balance 
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotal.innerText = newBalanceTotal;
 
-
-
     //clear deposit input field
     depositInput.value = '';
+
+})
+
+//handle Withdraw event handler
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    //get the amount withdrawn
+    const withdrawInput = document.getElementById('withdraw-input');
+    //converting input string to number
+    const withdawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdawAmountText);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+
+     //converting deposit input string to number
+     const previousWithdrawText = withdrawTotal.innerText;
+     const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+     //Update Total withdraw
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //Update Balance after Withdaw
+    const balanceTotal = document.getElementById('balance-total');
+
+    //converting balance input string to number
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    //Update Total balance
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    //clear deposit input field
+    withdrawInput.value = '';
 
 })
