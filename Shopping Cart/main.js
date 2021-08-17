c191129c46781657ae3808a9b506c268f66d9b31
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-17 22:40:27 +0600

    quantity input increment and decrement

---
+++ Shopping Cart/main.js
@@ -0,0 +1,15 @@
+document.getElementById('case-plus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber  = caseInput.value;
+    // string to number
+    caseInput.value = parseInt(caseNumber) + 1;
+    
+})
+
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber  = caseInput.value;
+    // string to number
+    caseInput.value = parseInt(caseNumber) - 1;
+    
+})
