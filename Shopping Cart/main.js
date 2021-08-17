c820102cfac833c520d596634e13c7acc0a1e77e
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-17 23:01:47 +0600

    quantity input increment and decrement change to function

--- Shopping Cart/main.js
+++ Shopping Cart/main.js
@@ -1,15 +1,23 @@
-document.getElementById('case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber  = caseInput.value;
-    // string to number
-    caseInput.value = parseInt(caseNumber) + 1;
-    
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        // string to number
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
 })
 
-document.getElementById('case-minus').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber  = caseInput.value;
-    // string to number
-    caseInput.value = parseInt(caseNumber) - 1;
-    
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 })
