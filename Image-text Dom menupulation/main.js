9916c9409912956f99140969038c6f67321b701c
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-20 14:36:04 +0600

    select troggle added with function

--- Image-text Dom menupulation/main.js
+++ Image-text Dom menupulation/main.js
@@ -13,29 +13,74 @@ const shippingField = document.getElementById('shipping');
 
 const total = document.getElementById('total');
 
-free.addEventListener('click', function(){
-   shippingField.innerText = '5';
+// Toggle button border color
+function isContain(elem){
+    //return is class present
+    return elem.classList.contains('selected');
+}
+
+free.addEventListener('click', function () {
+    if (!isContain(free)) {
+        free.classList.add('selected');
+        express.classList.remove('selected');
+    }
+    shippingField.innerText = '5';
+    updateTotal();
 });
-express.addEventListener('click', function(){});
-shippingField.innerText = '30';
+express.addEventListener('click', function () {
+    if (!isContain(express)) {
+        free.classList.remove('selected');
+        express.classList.add('selected');
+    }
+    shippingField.innerText = '30';
+    updateTotal();
+});
+
+//Update Total
+function updateTotal() {
+   const shippingCharge = Number(shippingField.innerText);
+   const productPrice = Number(price.innerText);
+   const grandTotal = shippingCharge + productPrice;
+   total.innerText = grandTotal;
+
+}
 
 div1.addEventListener('click', function () {
+    if (!isContain(div1)) {
+        div1.classList.add('selected');
+        div2.classList.remove('selected');
+        div3.classList.remove('selected');
+    }
     //Changing image
     img.src = 'images/1.jpg';
     //Changing description
     desc.innerText = 'First Lorem ipsum dolor sit amet consectetur, adipisicing elit.Rerum beatae laboriosam, commodi quis tempore necessitatibus.Voluptatibus minima aut non'
     //Changing price
-    price.innerText = '650$';
-   
-
+    price.innerText = '650';
+    // Update Total
+    updateTotal();
 });
 div2.addEventListener('click', function () {
+    if (!isContain(div2)) {
+        div1.classList.remove('selected');
+        div2.classList.add('selected');
+        div3.classList.remove('selected');
+    }
+
     img.src = 'images/2.jpg';
     desc.innerText = 'Second Lorem ipsum dolor sit amet consectetur, adipisicing elit.Rerum beatae laboriosam, commodi quis tempore necessitatibus.Voluptatibus minima aut non'
-    price.innerText = '700$';
+    price.innerText = '700';
+    updateTotal();
 });
 div3.addEventListener('click', function () {
+    if (!isContain(div3)) {
+        div1.classList.remove('selected');
+        div2.classList.remove('selected');
+        div3.classList.add('selected');
+    }
+
     img.src = 'images/3.jpg';
     desc.innerText = 'Third Lorem ipsum dolor sit amet consectetur, adipisicing elit.Rerum beatae laboriosam, commodi quis tempore necessitatibus.Voluptatibus minima aut non'
-    price.innerText = '600$';
+    price.innerText = '400';
+    updateTotal();
 });
