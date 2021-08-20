fe6fcda0ca2147820e047a14bc1428cd2375937a
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-20 20:43:10 +0600

    login error massge added

--- Functional Bank/main.js
+++ Functional Bank/main.js
@@ -11,4 +11,8 @@ document.getElementById('login-submit').addEventListener('click', function(){
     if (userEmail == 'xyz@gmail.com' && userPassword == 'secret'){
         window.location.href ='banking.html';
     }
+    else{
+        const errorMessage = document.getElementById('error-message')
+        errorMessage.style.display = 'block';
+    }
 })
