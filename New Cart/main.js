e78a686d3bb1211045f3f034bbccb015bd57f2bb
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-21 15:05:58 +0600

    All done except bonus

---
+++ New Cart/main.js
@@ -0,0 +1,105 @@
+//Default best price
+const price = document.getElementById('price');
+
+
+function isContain(element) {
+    //return is class present
+    return element.classList.contains('selected');
+}
+
+//Memory options
+const memory1 = document.getElementById('memory-8gb');
+const memory2 = document.getElementById('memory-16gb');
+
+const memoryField = document.getElementById('memory-cost');
+const totalOne = document.getElementById('total');
+
+memory1.addEventListener('click', function () {
+    //Memory options highlighted with select border
+    if (!isContain(memory1)) {
+        memory1.classList.add('selected');
+        memory2.classList.remove('selected');
+    }
+    memoryField.innerText = '0';
+    updateTotal();
+});
+memory2.addEventListener('click', function () {
+    if (!isContain(memory2)) {
+        memory1.classList.remove('selected');
+        memory2.classList.add('selected');
+    }
+    memoryField.innerText = '180';
+    updateTotal();
+});
+
+//Storage options
+const storage1 = document.getElementById('storage-256gb');
+const storage2 = document.getElementById('storage-512gb');
+const storage3 = document.getElementById('storage-1tb');
+
+const storageField = document.getElementById('storage-cost');
+const totalTwo = document.getElementById('total');
+
+storage1.addEventListener('click', function () {
+    //Storage options highlighted with select border
+    if (!isContain(storage1)) {
+        storage1.classList.add('selected');
+        storage2.classList.remove('selected');
+        storage3.classList.remove('selected');
+    }
+    storageField.innerText = '0';
+    updateTotal();
+});
+storage2.addEventListener('click', function () {
+    if (!isContain(storage2)) {
+        storage1.classList.remove('selected');
+        storage2.classList.add('selected');
+        storage3.classList.remove('selected');
+    }
+    storageField.innerText = '100';
+    updateTotal();
+});
+storage3.addEventListener('click', function () {
+    if (!isContain(storage3)) {
+        storage1.classList.remove('selected');
+        storage2.classList.remove('selected');
+        storage3.classList.add('selected');
+    }
+    storageField.innerText = '180';
+    updateTotal();
+});
+
+//Delivery options
+const free = document.getElementById('free');
+const fast = document.getElementById('fast');
+
+const shippingField = document.getElementById('shipping');
+const newTotal = document.getElementById('total');
+
+//delivery options highlighted with select border
+free.addEventListener('click', function () {
+    if (!isContain(free)) {
+        free.classList.add('selected');
+        fast.classList.remove('selected');
+    }
+    shippingField.innerText = '0';
+    updateTotal();
+});
+fast.addEventListener('click', function () {
+    if (!isContain(fast)) {
+        free.classList.remove('selected');
+        fast.classList.add('selected');
+    }
+    shippingField.innerText = '20';
+    updateTotal();
+});
+
+// new Update Total
+function updateTotal() {
+    const storageExtraCost = Number(storageField.innerText);
+    const memoryExtraCost = Number(memoryField.innerText);
+    const shippingCharge = Number(shippingField.innerText);
+    const productPrice = Number(price.innerText);
+    const GrandTotal = memoryExtraCost + productPrice + storageExtraCost + shippingCharge;
+    newTotal.innerText = GrandTotal;
+}
