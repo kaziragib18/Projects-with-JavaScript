172c82ce288b070baf7e69769248e549771c920f
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-24 16:44:55 +0600

    css link cdn added

--- Problem-solving/Es6-recap.js
+++ Problem-solving/Es6-recap.js
@@ -22,7 +22,7 @@ const square = x => x * x;
 console.log(square(9));
 
 
-// template-string
+//               template-string
 
 const priya = 'Asif Akbar';
 const meye = "Meye tumi ki dukkho chino";
@@ -53,7 +53,7 @@ console.log(fullNew);
 
 
 
-//distructuring
+//            distructuring
 const fish = { id: 58, name: 'King Hilsha', price: 9000, phone: '01717555555555', address: 'Chandpur', dress: 'silver' };
 
 // const phone = fish.phone;
@@ -91,3 +91,32 @@ const { work, framework } = company.web;
 const { food } = company.ceo;
 const { second, third } = company.web.tech;
 console.log(work, framework, food);
+
+
+//           Array Map
+const numbers = [4, 6, 8, 10];
+const output2 = [];
+
+// function doubleOld(number) {
+//     return number * 2;
+// }
+
+const doubleIt = number => number * 2;
+
+for (const number of numbers) {
+    const result = doubleIt(number);
+    output2.push(result);
+}
+// console.log(output2);
+
+// 1. loop through each element
+// 2. for each element call the provided function
+// 3. result for each element will be stored in an array
+
+// const output = numbers.map(doubleIt);
+// const output = numbers.map(number => number * 2);
+const output = numbers.map(x => x * 2);
+// console.log(output);
+
+const squares = numbers.map(x => x * x);
+console.log(squares);
