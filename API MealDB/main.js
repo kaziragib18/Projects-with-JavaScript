30443b5353e14f70685c739184c12350b09644a0
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-29 14:43:42 +0600

    add card and search function added

--- API MealDB/main.js
+++ API MealDB/main.js
@@ -1,12 +1,32 @@
 const searchFood = () => {
    const searchField = document.getElementById('search-field');
    const searchText = searchField.value;
-//    console.log(searchText);
+   //    console.log(searchText);
 
    searchField.value = '';
    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-//    console.log(url);
-fetch(url)
-.then(res => res.json())
-.then(data => console.log(data.meals));
+   //    console.log(url);
+   fetch(url)
+      .then(res => res.json())
+      .then(data => displaySearchResult(data.meals));
+}
+
+const displaySearchResult = meals => {
+   // console.log(meals);
+   const searResult = document.getElementById('search-result');
+   meals.forEach(meal => {
+      console.log(meal);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
+      <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text p-1 text-wrap text-break"  style="width: 24rem;">${meal.strInstructions.slice(0, 250)}</p>
+            </div>
+      </div>
+      `;
+      searResult.appendChild(div);
+   })
 }
