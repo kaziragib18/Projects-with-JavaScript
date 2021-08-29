e1b21b5dd880b2008926efb6afe406f846a3c32c
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-08-29 15:36:22 +0600

    view details in another card

--- API MealDB/main.js
+++ API MealDB/main.js
@@ -15,18 +15,40 @@ const displaySearchResult = meals => {
    // console.log(meals);
    const searResult = document.getElementById('search-result');
    meals.forEach(meal => {
-      console.log(meal);
+      // console.log(meal);
       const div = document.createElement('div');
       div.classList.add('col');
       div.innerHTML = `
-      <div class="card h-100">
+      <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text p-1 text-wrap text-break"  style="width: 24rem;">${meal.strInstructions.slice(0, 250)}</p>
+                    <p class="card-text p-1">${meal.strInstructions.slice(0, 250)}</p>
             </div>
       </div>
       `;
       searResult.appendChild(div);
    })
+}
+const loadMealDetail = mealId => {
+   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+   fetch(url)
+       .then(res => res.json())
+       .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+   console.log(meal);
+   const mealDetails = document.getElementById('meal-details');
+   const div = document.createElement('div');
+   div.classList.add('card');
+   div.innerHTML = `
+   <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+   <div class="card-body">
+       <h5 class="card-title">${meal.strMeal}</h5>
+       <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+       <a href="${meal.strYoutube}" class="btn btn-primary">How it made</a>
+   </div>
+   `;
+   mealDetails.appendChild(div);
 }
