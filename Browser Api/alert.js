b33225d19cb965b9b60aff115366d2ca268d5c49
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-05 19:11:46 +0600

    expolre js alert and confirm

---
+++ Browser Api/alert.js
@@ -0,0 +1,17 @@
+// console.log('hi')
+// alert('hide now!!')
+const godZilla = () => {
+      alert('its here get the bazuka!!');
+}
+
+const airStrike = () => {
+      const response = confirm('Target locked. Permission to fire?');
+      console.log(response);
+      
+      if(response === true){
+            alert('Missile has been launched!');
+      }
+      else{
+            alert('Rojar that! launch order on hold');
+      }
+}
---
+++ Browser Api/explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {
+      name: 'Ragib', job: 'Software Developer',
+      makeMoivie: function (name) {
+            console.log(name);
+      }
+};
+student.makeMoivie('Travel around')
+// document.getElementById('name');
