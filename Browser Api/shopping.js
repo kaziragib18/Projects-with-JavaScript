a55c7a23dffafb6d2524e105e27bf06334407feb
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-06 22:54:32 +0600

    use of prompt

--- Browser Api/shopping.js
+++ Browser Api/shopping.js
@@ -1,10 +1,9 @@
 const displayLocalStorageCart = () => {
       const cart = getCart();
-      for(const name in cart) {
+      for (const name in cart) {
             displayProduct(name);
       }
 }
-
 const addItem = () => {
       const nameField = document.getElementById('product-name');
       const name = nameField.value;
@@ -25,20 +24,25 @@ const displayProduct = name => {
       ul.appendChild(li);
 }
 
-const getCart = () =>{
-    const cart = localStorage.getItem('cart');
-    let cartObj;
-    if(cart){
-          cartObj = JSON.parse(cart);
-    } 
-    else{
-          cartObj = {};
-    }
-    return cartObj;
+const getCart = () => {
+      const cart = localStorage.getItem('cart');
+      let cartObj;
+      if (cart) {
+            cartObj = JSON.parse(cart);
+      }
+      else {
+            cartObj = {};
+      }
+      return cartObj;
 }
 const addProductToCart = name => {
       const cart = getCart();
-      cart[name] = 1;
+      if (cart[name]) {
+            cart[name] = cart[name] + 1;
+      }
+      else{
+            cart[name] = 1;
+      }
       // console.log(cart);
       const cartStringiFied = JSON.stringify(cart);
       localStorage.setItem('cart', cartStringiFied);
