0a7b938bc0fb0acc24550252c3de5ada4e54ff89
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2022-02-24 23:29:54 +0600

    find all duplicates in array

--- Data-Structure-Problem-solving/findAllDuplicates.js
+++ Data-Structure-Problem-solving/findAllDuplicates.js
@@ -3,3 +3,17 @@
 // Input: nums = [4,3,2,7,8,2,3,1]
 // Output: [2,3]
 
+var findDuplicates = function (nums) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let element = Math.abs(nums[i] - 1);
+    let value = nums[element];
+    if (value < 0) {
+      result.push(Math.abs(nums[1]));
+    } else {
+      nums[element] = -nums[element]
+    }
+
+  }
+  return result;
+}
